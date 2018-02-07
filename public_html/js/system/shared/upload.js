705dbdcd1a4c0a081f39b8a12b32b628fdbaaa1c
a022593391p <a022593391p@S09-PC03>
2018-02-07 13:40:10 +0100

    pruebas de upload

---
+++ public_html/js/system/shared/upload.js
@@ -0,0 +1,16 @@
+$scope.add = function() {
+    var f = document.getElementById('file').files[0],
+        r = new FileReader();
+
+    r.onloadend = function(e) {
+      var data = e.target.result;
+      //send your binary data via $http or $resource or do anything else with it
+    }
+
+    r.readAsBinaryString(f);
+}
+
+/**
+<!--<input type="file" id="file" name="file"/>
+<button ng-click="add()">Add</button>-->
+*/
